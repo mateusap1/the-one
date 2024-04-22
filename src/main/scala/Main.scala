c908155de880f3bf3cadc7b25cff6f71dfcda65f
Mateus Santos <santos.mateus@protonmail.com>
2024-04-22 14:08:03 -0300

    Drop files from .gitignore

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,82 +3,100 @@ package org.unb.theone
 import scala.io.Source
 import scala.collection.immutable.{Set, Map}
 
-class TheOne[T](var value: T) {
-  def bind(fun: T => T) = {
-    value = fun(value)
-    value
+sealed trait TheOneData
+
+case class TheOneRaw(raw: String) extends TheOneData
+case class TheOneWords(words: List[String]) extends TheOneData
+case class TheOneWordsMap(wordsMap: Map[String, Int]) extends TheOneData
+case class TheOneWordsFlatten(wordsFlatten: List[(String, Int)])
+    extends TheOneData
+
+class TheOne(var value: TheOneData) {
+  def bind[A <: TheOneData, B <: TheOneData](f: A => B): TheOne = {
+    value = f(value.asInstanceOf[A])
+    this
   }
 
-  def printme = println(value)
+  def printme = {
+    value match {
+      case TheOneRaw(raw) => println(raw)
+      case TheOneWords(words) => println(words)
+      case TheOneWordsMap(wordsMap) => println(wordsMap)
+      case TheOneWordsFlatten(wordsFlatten) => println(wordsFlatten)
+    }
+  }
 }
 
 object WordCounter {
-  def readFile(path: String): String = {
-    Source.fromFile(path).mkString
+  def readFile(path: TheOneRaw): TheOneRaw = {
+    TheOneRaw(Source.fromFile(path.raw).mkString)
   }
 
-  def filterChars(inputString: String): String = {
-    inputString.replaceAll("""[\W_]+""", " ")
+  def filterChars(inputString: TheOneRaw): TheOneRaw = {
+    TheOneRaw(inputString.raw.replaceAll("""[\W_]+""", " "))
   }
 
-  def normalize(inputString: String): String = {
-    inputString.toLowerCase()
+  def normalize(inputString: TheOneRaw): TheOneRaw = {
+    TheOneRaw(inputString.raw.toLowerCase())
   }
 
-  def scan(inputString: String): List[String] = {
-    inputString.split(" ").toList
+  def scan(inputString: TheOneRaw): TheOneWords = {
+    TheOneWords(inputString.raw.split(" ").toList)
   }
 
-  def removeStopWords(
-      inputWords: List[String],
-      stopWords: Set[String]
-  ): List[String] = {
-    inputWords.filter((s: String) => !stopWords.contains(s))
+  def removeStopWords(inputWords: TheOneWords): TheOneWords = {
+    val stopWordsPath =
+      "/Users/mateusoliveirasantos/Documents/estudos/unb/tec_prog/code/the-one/stop_words.txt"
+    val stopWords = Source.fromFile(stopWordsPath).getLines().toSet
+
+    TheOneWords(inputWords.words.filter((s: String) => !stopWords.contains(s)))
   }
 
-  def addToMap(s: String, acc: Map[String, Int]) = {
-    if (acc.contains(s)) {
-      acc + (s -> (acc(s) + 1))
-    } else {
-      acc + (s -> 1)
+  def frequencies(inputWords: TheOneWords): TheOneWordsMap = {
+    def addToMap(s: String, acc: Map[String, Int]) = {
+      if (acc.contains(s)) {
+        acc + (s -> (acc(s) + 1))
+      } else {
+        acc + (s -> 1)
+      }
     }
+
+    TheOneWordsMap(inputWords.words.foldRight(Map.empty[String, Int])(addToMap))
   }
 
-  def frequencies(inputWords: List[String]): Map[String, Int] = {
-    inputWords.foldRight(Map.empty[String, Int])(addToMap)
-  }
-
-  def sortFreq(frequencyWords: Map[String, Int]): List[(String, Int)] = {
-    frequencyWords.toList.sortBy[Int]((wordFreq: (String, Int)) => -wordFreq._2)
-  }
-
-  def topFreqs(ordFreqWords: List[(String, Int)], num: Int) = {
-    ordFreqWords
-      .slice(0, num)
-      .foldRight("")((wordFreq: (String, Int), acc: String) =>
-        wordFreq._1 + " - " + wordFreq._2.toString + "\n" + acc
+  def sortFreq(frequencyWords: TheOneWordsMap): TheOneWordsFlatten = {
+    TheOneWordsFlatten(
+      frequencyWords.wordsMap.toList.sortBy[Int]((wordFreq: (String, Int)) =>
+        -wordFreq._2
       )
+    )
+  }
+
+  def topFreqs(ordFreqWords: TheOneWordsFlatten): TheOneRaw = {
+    TheOneRaw(
+      ordFreqWords.wordsFlatten
+        .slice(0, 25)
+        .foldRight("")((wordFreq: (String, Int), acc: String) =>
+          wordFreq._1 + " - " + wordFreq._2.toString + "\n" + acc
+        )
+    )
   }
 }
 
 object Main extends App {
   val path =
-    "/Users/mateusoliveirasantos/Documents/estudos/unb/tec_prog/code/the-one"
+    "/Users/mateusoliveirasantos/Documents/estudos/unb/tec_prog/code/the-one/pride-and-prejudice.txt"
   // val content = TheOne.readFile("./test.txt")
   // printf("File content: %s", content)
   // println(WordCounter.addToMap("a", Map("a" -> 1, "b" -> 2)))
-  type TheOneType =
-    Either[Either[String, List[String]], Either[Map[String, Int], List[
-      (String, Int)
-    ]]]
-  new TheOne[TheOneType](path)
+  new TheOne(TheOneRaw(path))
     .bind(WordCounter.readFile(_))
     .bind(WordCounter.filterChars(_))
     .bind(WordCounter.normalize(_))
     .bind(WordCounter.scan(_))
     .bind(WordCounter.removeStopWords(_))
-    .bind(WordCounter.normalize(_))
-    .bind(WordCounter.normalize(_))
-    .bind(WordCounter.normalize(_))
-
+    .bind(WordCounter.frequencies(_))
+    .bind(WordCounter.sortFreq(_))
+    .bind(WordCounter.topFreqs(_))
+    .printme
 }
